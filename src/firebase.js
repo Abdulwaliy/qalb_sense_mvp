9005f946a2c28936efc1b9e92eb5667e7d312f7f
unknown <aoyekunle@student.oauife.edu.ng>
2025-06-28 18:26:39 +0100

    Add Firebase config and imports

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDF7l1iX8DMp95JhHTvP1h3ZH0lyrCkA98",
+  authDomain: "qalbsense-mvp.firebaseapp.com",
+  projectId: "qalbsense-mvp",
+  storageBucket: "qalbsense-mvp.firebasestorage.app",
+  messagingSenderId: "1099108125316",
+  appId: "1:1099108125316:web:986a79dcf4fd73481190d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
